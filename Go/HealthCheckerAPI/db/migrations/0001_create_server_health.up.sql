1bedddf01082eb80b41c22576cc06f323991c4d8
DevByAnton <root@DESKTOP-2CS4P4N.>
2025-06-07 23:27:36 +0200

    Initial Commit

--- Go/HealthCheckerAPI/db/migrations/0001_create_server_health.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS server_health (
-	id SERIAL PRIMARY KEY,
-	server_id VARCHAR(255) NOT NULL,
-	cpu_usage INT,
-	memory_usage INT,
-	disk_usage INT,
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
